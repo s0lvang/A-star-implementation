bf97b9d035e7a9a68516d8cea8a5db1dad9fb3e3
s0lvang <august.s.solvang@gmail.com>
2018-09-26 13:24:15 +0200

    fix lint

--- Node.py
+++ Node.py
@@ -1,8 +1,9 @@
 class Node:
-    
-    def __init__(self,weight,isStartNode,isEndNode):
+
+    def __init__(self, weight, isStartNode, isEndNode):
         self.weight = weight
-        self.isStartNode=isStartNode
-        self.isEndNode=isEndNode
+        self.isStartNode = isStartNode
+        self.isEndNode = isEndNode
+
     def __repr__(self):
         return str(self.weight)
--- boardReader.py
+++ boardReader.py
@@ -1,17 +1,17 @@
 from Node import Node
 from math import inf
 
-def generateValue(element):
-    if(element=='#'):
-        return Node(inf, False, False)
-    elif (element=='.'):
-        return Node(1,False,False)
-    elif (element=='A'):
-        return Node(0,True,False)
-    elif(element=='B'):
-        return Node(0,False,True)
-    return Node(element,False,False)
 
+def generateValue(element):
+    if(element == '#'):
+        return Node(inf, False, False)
+    elif (element == '.'):
+        return Node(1, False, False)
+    elif (element == 'A'):
+        return Node(0, True, False)
+    elif(element == 'B'):
+        return Node(0, False, True)
+    return Node(element, False, False)
 
 
 def makeBoard(filename):
@@ -20,9 +20,7 @@ def makeBoard(filename):
         lines = f.readlines()
     for i in range(len(lines)):
         lines[i] = list(lines[i].strip())
-        lines[i] = list(map(lambda x:generateValue(x),lines[i]))
-    print(lines)
-
-
+        lines[i] = list(map(lambda x: generateValue(x), lines[i]))
+    return lines
 
 makeBoard("boards/board-1-1.txt")
