bf3d5ac75e5315d4400bf2b925f7d72344337e01
Aslak Hollund <aslakhol@gmail.com>
2018-09-27 22:28:45 +0200

    add display for task 1

---
+++ display2.py
@@ -0,0 +1,56 @@
+from tkinter import *
+from a_star import main, get_path
+from boardReader import get_node_list
+from math import inf
+
+
+class Display:
+
+    def __init__(self, master):
+
+        self.master = master
+
+        # Creating frame for buttons
+        frame = Frame(master)
+        frame.pack()
+
+        # Creating button
+        self.quit_button = Button(frame, text="QUIT", command=frame.quit)
+        self.quit_button.pack(side=LEFT)
+
+        self.color_dict = {
+            inf: "black",
+            100: "blue",
+            50: "gray",
+            10: "forest green",
+            5: "lawn green",
+            1: "brown",
+        }
+
+    def setup_canvas(self, master, width, height):
+        self.canvas = Canvas(master, width=width, height=height)
+        self.canvas.pack()
+
+    def paint_gui(self):
+        node_list = get_node_list()
+        path = get_path()
+        number_in_grid = 0
+        for y in range(0, 100, 10):
+            for x in range(0, 400, 10):
+                fill_color = self.color_dict.get(node_list[number_in_grid])
+                if (y // 10, x // 10) in path:
+                    fill_color = "gold"
+                number_in_grid += 1
+                self.canvas.create_rectangle(x, y, x + 10, y + 10, fill=fill_color, outline="black")
+
+
+# Declaring tkinter root
+root = Tk()
+
+main('boards/board-2-4.txt')
+
+display = Display(root)
+display.setup_canvas(root, 400, 100)
+display.paint_gui()
+
+root.mainloop()
